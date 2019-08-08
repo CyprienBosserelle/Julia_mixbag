688f53a04ecacb5431a6c718cebcc08ecb12aaa1
Cyprien <cyprien.bosserelle@gmail.com>
2019-08-08 22:08:14 +1200

    Update ut_reconstr.jl

--- ut_reconstr.jl
+++ ut_reconstr.jl
@@ -1,4 +1,207 @@
 
+using Dates
+
+function  ut_astron(jd)
+    # % UT_ASTRON()
+    # % calculate astronomical constants
+    # % input
+    # %   jd = time [datenum UTC] (1 x nt)
+    # % outputs
+    # %   astro = matrix [tau s h p np pp]T, units are [cycles] (6 x nt)
+    # %   ader = matrix of derivatives of astro [cycles/day] (6 x nt)
+    # % UTide v1p0 9/2011 d.codiga@gso.uri.edu
+    # % (copy of t_astron.m from t_tide, Pawlowicz et al 2002)
+    nt=length(jd);
+    jd=collect(DateTime(2009,11,30,12,0,0):Dates.Hour(1):DateTime(2009,12,31,12,0,0));
+
+    d=datetime2julian.(jd).-datetime2julian(DateTime(1899,12,31,12,0,0));
+    D=d./10000;
+    args=[ones(nt),
+          d,
+          D.*D,
+          D.^3]
+    sc= [ 270.434164,13.1763965268,-0.0000850, 0.000000039];
+    hc= [ 279.696678, 0.9856473354, 0.00002267,0.000000000];
+    pc= [ 334.329556, 0.1114040803,-0.0007739,-0.00000026];
+    npc=[-259.183275, 0.0529539222,-0.0001557,-0.000000050];
+    ppc=[ 281.220844, 0.0000470684, 0.0000339, 0.000000070];
+
+    astro=zeros(6,nt)
+
+    #astro=rem( [sc,hc,pc,npc,ppc].*args./360.0 ,1);
+    astro[2,:]=rem.( sc*args'./360.0 ,1);
+    astro[3,:]=rem( hc.*args./360.0 ,1);
+    astro[4,:]=rem( pc.*args./360.0 ,1);
+    astro[5,:]=rem( npc.*args./360.0 ,1);
+    astro[6,:]=rem( ppc.*args./360.0 ,1);
+
+
+
+    tau=rem(jd(:)',1)+astro[3,:]-astro[2,:];
+    astro[1,:]=tau;
+    dargs=[zeros(size(jd));
+           ones(size(jd));
+           2.0e-4.*D;
+           3.0e-4.*D.*D];
+    ader=[sc;hc;pc;npc;ppc]*dargs./360.0;
+    dtau=1.0+ader(2,:)-ader(1,:);
+    ader=[dtau;ader];
+
+    return astro,ader
+end
+
+function [F,U,V] = ut_FUV(t,tref,lind,lat,ngflgs)
+    # % UT_FUV()
+    # % compute nodal/satellite correction factors and astronomical argument
+    # % inputs
+    # %   t = times [datenum UTC] (nt x 1)
+    # %   tref = reference time [datenum UTC] (1 x 1)
+    # %   lind = list indices of constituents in ut_constants.mat (nc x 1)
+    # %   lat = latitude [deg N] (1 x 1)
+    # %   ngflgs = [NodsatLint NodsatNone GwchLint GwchNone] each 0/1
+    # % output
+    # %   F = real nodsat correction to amplitude [unitless] (nt x nc)
+    # %   U = nodsat correction to phase [cycles] (nt x nc)
+    # %   V = astronomical argument [cycles] (nt x nc)
+    # % UTide v1p0 9/2011 d.codiga@gso.uri.edu
+    # % (uses parts of t_vuf.m from t_tide, Pawlowicz et al 2002)
+
+    nt = length(t);
+    nc = length(lind);
+    # nodsat
+    if ngflgs(2) # none
+        F = ones(nt,nc);
+        U = zeros(nt,nc);
+    else
+        if ngflgs(1) # linearized times
+            #tt = tref;
+        else         # exact times
+            tt = t;
+        end
+        ntt = length(tt);
+        load('ut_constants.mat');
+        [astro,~]=ut_astron(tt');
+        if abs(lat)<5
+            lat=sign(lat).*5;
+        end
+        slat=sin(pi*lat/180);
+        rr=sat.amprat;
+        j=find(sat.ilatfac==1);
+        rr(j)=rr(j).*0.36309.*(1.0-5.0.*slat.*slat)./slat;
+        j=find(sat.ilatfac==2);
+        rr(j)=rr(j).*2.59808.*slat;
+        uu=rem( sat.deldood*astro(4:6,:)+sat.phcorr(:,ones(1,ntt)), 1);
+        nfreq=length(const.isat); %#ok
+        mat = rr(:,ones(1,ntt)).*exp(1i*2*pi*uu);
+        F = ones(nfreq,ntt);
+        ind = unique(sat.iconst);
+        for i = 1:length(ind)
+            F(ind(i),:) = 1+sum(mat(sat.iconst==ind(i),:),1);
+        end
+        U = imag(log(F))/(2*pi); % faster than angle(F)
+        F=abs(F);
+        for k=find(isfinite(const.ishallow))'
+            ik=const.ishallow(k)+(0:const.nshallow(k)-1);
+            j = shallow.iname(ik);
+            exp1 = shallow.coef(ik);
+            exp2 = abs(exp1);
+            F(k,:)=prod(F(j,:).^exp2(:,ones(ntt,1)),1);
+            U(k,:)=sum(U(j,:).*exp1(:,ones(ntt,1)),1);
+        end
+        F=F(lind,:)';
+        U=U(lind,:)';
+        if ngflgs(1) % nodal/satellite with linearized times
+            F = F(ones(nt,1),:);
+            U = U(ones(nt,1),:);
+        end
+    end
+    # gwch (astron arg)
+    if ngflgs(4) % none (raw phase lags not greenwich phase lags)
+        if ~exist('const','var')
+            load('ut_constants.mat','const');
+        end
+        [~,ader] = ut_astron(tref);
+        ii=isfinite(const.ishallow);
+        const.freq(~ii) = (const.doodson(~ii,:)*ader)/(24);
+        for k=find(ii)'
+            ik=const.ishallow(k)+(0:const.nshallow(k)-1);
+            const.freq(k)=sum(const.freq(shallow.iname(ik)).*shallow.coef(ik));
+        end
+        V = 24*(t-tref)*const.freq(lind)';
+    else
+        if ngflgs(3)  # linearized times
+            tt = tref;
+        else
+            tt = t;   # exact times
+        end
+        ntt = length(tt);
+        if exist('astro','var')
+            if ~isequal(size(astro,2),ntt)
+                [astro,~]=ut_astron(tt');
+            end
+        else
+            [astro,~]=ut_astron(tt');
+        end
+        if ~exist('const','var')
+            load('ut_constants.mat');
+        end
+        V=rem( const.doodson*astro+const.semi(:,ones(1,ntt)), 1);
+        for k=find(isfinite(const.ishallow))'
+            ik=const.ishallow(k)+(0:const.nshallow(k)-1);
+            j = shallow.iname(ik);
+            exp1 = shallow.coef(ik);
+            V(k,:) = sum(V(j,:).*exp1(:,ones(ntt,1)),1);
+        end
+        V=V(lind,:)';
+        if ngflgs(3)    % linearized times
+            [~,ader] = ut_astron(tref);
+            ii=isfinite(const.ishallow);
+            const.freq(~ii) = (const.doodson(~ii,:)*ader)/(24);
+            for k=find(ii)'
+                ik=const.ishallow(k)+(0:const.nshallow(k)-1);
+                const.freq(k)=sum( const.freq(shallow.iname(ik)).* ...
+                    shallow.coef(ik) );
+            end
+            V = V(ones(1,nt),:) + 24*(t-tref)*const.freq(lind)';
+        end
+    end
+end
+
+function ut_E(t,tref,frq,lind,lat,ngflgs,prefilt)
+    # UT_E()
+    # compute complex exponential basis function
+    # inputs
+    #   t = times [datenum UTC] (nt x 1)
+    #   tref = reference time [datenum UTC] (1 x 1)
+    #   frq = frequencies [cph] (nc x 1)
+    #   lind = list indices of constituents in ut_constants.mat (nc x 1)
+    #   lat = latitude [deg N] (1 x 1)
+    #   ngflgs = [NodsatLint NodsatNone GwchLint GwchNone] each 0/1
+    #       ([0 1 0 1] case not allowed, and not needed, in ut_E)
+    #   prefilt = 'prefilt' input to ut_solv
+    # output
+    #   E = complex exponential basis function [unitless] (nt x nc)
+    # UTide v1p0 9/2011 d.codiga@gso.uri.edu
+
+    nt = length(t);
+    nc = length(lind);
+    if ngflgs(2) && ngflgs(4)
+        F = ones(nt,nc);
+        U = zeros(nt,nc);
+        V = 24*(t-tref)*frq';
+    else
+        [F,U,V] = ut_FUV(t,tref,lind,lat,ngflgs);
+    end
+    E = F.*exp(1im*(U+V)*2*pi);
+    if ~isempty(prefilt)
+        # P=interp1(prefilt.frq,prefilt.P,frq)';
+        # P( P>max(prefilt.rng) | P<min(prefilt.rng) | isnan(P) )=1;
+        # E = E.*P(ones(nt,1),:);
+    end
+    return E
+end
+
+
 # first build a constituent object
 struct cnstit
     #constituent parameter
@@ -25,7 +228,7 @@ end
 #Input should be an array of cnstit
 coef=Vector{cnstit}(undef,5)
 
-opt=option(false,2.0,0.0,false,false)
+opt=option(false,2.0,0.0,false,false,false,false)
 
 
 coef[1]=cnstit("M2",0.493138384657320,0.000588412284822346,206.472841113229,0.0651156148667687,0.0805114006717706,48)
@@ -54,11 +257,11 @@ coef[5]=cnstit("O1",0.0319278720784309,0.000901721376152978,234.835289824733,1.6
 
 # determine constituents to include
 if !isempty([])
-    [~,ind] = ismember(cellstr(opt.cnstit),coef.name);
-    if ~isequal(length(ind),length(cellstr(opt.cnstit)))
-        error(['ut_reconstr: one or more of input constituents Cnstit '...
-            'not found in coef.name']);
-    end
+    # [~,ind] = ismember(cellstr(opt.cnstit),coef.name);
+    # if ~isequal(length(ind),length(cellstr(opt.cnstit)))
+    #     error(['ut_reconstr: one or more of input constituents Cnstit '...
+    #         'not found in coef.name']);
+    # end
 else
     SNR=zeros(length(coef));
     PE=zeros(length(coef));
@@ -83,8 +286,8 @@ end
 # complex coefficients
 rpd = pi/180;
 if opt.twodim
-    ap = 0.5*(coef.Lsmaj(ind) + coef.Lsmin(ind)) .* exp(1i*(coef.theta(ind) - coef.g(ind))*rpd);
-    am = 0.5*(coef.Lsmaj(ind) - coef.Lsmin(ind)) .* exp(1i*(coef.theta(ind) + coef.g(ind))*rpd);
+    # ap = 0.5*(coef.Lsmaj(ind) + coef.Lsmin(ind)) .* exp(1i*(coef.theta(ind) - coef.g(ind))*rpd);
+    # am = 0.5*(coef.Lsmaj(ind) - coef.Lsmin(ind)) .* exp(1i*(coef.theta(ind) + coef.g(ind))*rpd);
 else
     ap=zeros(ComplexF64,length(coef))
     for el=1:length(coef)
