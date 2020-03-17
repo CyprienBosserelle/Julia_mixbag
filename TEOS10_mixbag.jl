683c9b09f0e02e55031289805355b8bc674aeb2e
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2020-03-17 15:08:24 +1300

    Added comments to D0sat calc

--- TEOS10_mixbag.jl
+++ TEOS10_mixbag.jl
@@ -11,26 +11,30 @@ module TEOS10_mixbag
         pt68 = pt .* 1.00024; # pt68 is the potential temperature in degress C on
                       # the 1968 International Practical Temperature Scale IPTS-68.
         gsw_T0 = 273.15;
+        #   Calculate Ts from T (deg C)
         y = log((298.15 - pt68) ./ (gsw_T0 + pt68));
 
         # The coefficents below are from the second column of Table 1 of Garcia and
-        # Gordon (1992)
+        # Gordon (1992) in umol/kg
         a0 =  5.80871;
         a1 =  3.20291;
         a2 =  4.17887;
         a3 =  5.10006;
         a4 = -9.86643e-2;
         a5 =  3.80369;
+
         b0 = -7.01577e-3;
         b1 = -7.70028e-3;
         b2 = -1.13864e-2;
         b3 = -9.51519e-3;
-        c0 = -2.75915e-7;
 
+        c0 = -2.75915e-7;
+        # in umol/kg
         O2sol = exp(a0 + y .* (a1 + y .* (a2 + y .* (a3 + y .* (a4 + a5 * y)))) + x .* (b0 + y .* (b1 + y .* (b2 + b3 * y)) + c0 * x));
 
         return O2sol;
     end
+
 end
 # function gsw_SP_from_SA(SA,p,long,lat)
     #gsw_SP_from_SA                  Practical Salinity from Absolute Salinity
